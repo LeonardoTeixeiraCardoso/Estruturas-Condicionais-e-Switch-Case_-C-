d3a8e9e15271084ea1fbb7308c75a048bb5b255e
Leonardo Teixeira Cardoso <leonardoteixeiracardoso01@gmail.com>
2025-11-17 14:19:01 -0300

    Verificação dos meses do ano

---
+++ Calendário_Mês.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int numero; 
+        
+    cout << "Digite um numero de 1 ao 12: ";
+    cin >> numero;
+    
+    switch (numero){
+        case 1 :
+            cout << "janeiro";
+        break;   
+            
+        case 2 :
+            cout << "fevereiro";
+        break;   
+        
+        case 3 :
+            cout << "março";
+        break;   
+        
+        case 4 :
+            cout << "abril";
+        break;   
+        
+        case 5 :
+            cout << "maio";
+        break;   
+        
+        case 6 :
+            cout << "junho";
+        break;   
+        
+        case 7 :
+            cout << "julho";
+        break;   
+        
+        case 8 :
+            cout << "agosto";
+        break;   
+        
+        case 9 :
+            cout << "setembro";
+        break;   
+        
+        case 10 :
+            cout << "outubro";
+        break;   
+        
+        case 11 :
+            cout << "novembro";
+        break;   
+        
+        case 12 :
+            cout << "dezembro";
+        break;   
+        
+        default : cout << "mes inexistente";
+    }
+
+    return 0;
+}
+
+
